773d0440c030dd64eb0d34969649f2bb692d2ef9
Daniil Kryshtofik <playmeharder@gmail.com>
2021-02-28 13:10:37 +0300

    project task done

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+"use strict";
+
+const movieDB = {
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
+};
+
+const adv = document.querySelectorAll(".promo__adv"),
+  genre = document.querySelector(".promo__genre"),
+  backGround = document.querySelector(".promo__bg"),
+  moviesArr = document.querySelectorAll(".promo__interactive-item");
+
+adv.forEach((element) => element.remove());
+
+genre.textContent = "ДРАМА";
+
+backGround.style.backgroundImage = "url(../img/bg.jpg)";
+
+movieDB.movies.sort();
+
+const sortedMovies = [...movieDB.movies];
+
+moviesArr.forEach(
+  (element, i) => (element.innerText = `${i + 1}. ${sortedMovies[i]}`)
+);
